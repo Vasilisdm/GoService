1d22b5f44cd544b8fe53660b9edd5c5179944dd6
vasilisdmr <v.dimitriou@katanox.com>
2023-01-17 22:10:34 +0200

    Assign the handler to the endpoint.

--- main.go
+++ main.go
@@ -20,6 +20,12 @@ var albums = []album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.Run("localhost:8080")
+}
+
 func getAlbums(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, albums)
 }
